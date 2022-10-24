e0e11be74ed8292ab8498855f25add4c4314edc7
Lucas Alexandre <lucasalexandre.s.cc@gmail.com>
2022-10-23 21:06:10 -0300

    chore: init project

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/**/*ts"],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  roots: ["<rootDir>/src"],
+  //setupFilesAfterEnv: ["./src/config/setup-tests.ts"],
+  testEnvironment: "node",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+};
